c1e0012b7b409ee0c1ace65752fae181c27ca373
Ryle Edwards <78249747+ryleedwards@users.noreply.github.com>
2023-10-06 18:05:31 -0500

    Add logic for retrieving league api data

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import { useState } from 'react';
+import getLeagueTeams from './services/getLeagueTeamsService';
+import clearLocalLeagueData from './services/clearLocalLeagueData';
 
 function App() {
-  return <></>;
+  const [leagueData, setLeagueData] = useState();
+
+  async function handleRetrieveDataClick() {
+    const data = await getLeagueTeams();
+    setLeagueData(data.response);
+  }
+
+  return (
+    <div>
+      <button onClick={handleRetrieveDataClick}>Retrieve Data</button>
+      <button onClick={clearLocalLeagueData}>Clear League Data</button>
+    </div>
+  );
 }
 
 export default App;
