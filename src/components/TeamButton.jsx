e3020e238227dc2f04847ca4dafd5c5e8ae85c0a
Ryle Edwards <78249747+ryleedwards@users.noreply.github.com>
2023-10-06 21:51:15 -0500

    Fix dependency in fetchLogo effect

--- src/components/TeamButton.jsx
+++ src/components/TeamButton.jsx
@@ -1,18 +1,18 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 
 export default function TeamButton({ team }) {
   const [logoImg, setLogoImg] = useState();
 
-  const fetchLogo = async () => {
+  const fetchLogo = useCallback(async () => {
     const result = await fetch(team.logoUrl);
     const imageBlob = await result.blob();
     const imageObjectUrl = URL.createObjectURL(imageBlob);
     setLogoImg(imageObjectUrl);
-  };
+  }, [team.logoUrl]);
 
   useEffect(() => {
     fetchLogo();
-  });
+  }, [fetchLogo]);
 
   return (
     <>
