34d560987a328f0c110c826284e53a40ec9a55c6
Ryle Edwards <78249747+ryleedwards@users.noreply.github.com>
2023-10-06 21:26:19 -0500

    Fetch team logo from provided URL

--- src/components/TeamButton.jsx
+++ src/components/TeamButton.jsx
@@ -1,7 +1,24 @@
+import { useState, useEffect } from 'react';
+
 export default function TeamButton({ team }) {
+  const [logoImg, setLogoImg] = useState();
+
+  const fetchLogo = async () => {
+    const result = await fetch(team.logoUrl);
+    const imageBlob = await result.blob();
+    const imageObjectUrl = URL.createObjectURL(imageBlob);
+    setLogoImg(imageObjectUrl);
+  };
+
+  useEffect(() => {
+    fetchLogo();
+  });
+
   return (
     <>
-      <button>{team.name}</button>
+      <button>
+        <img src={logoImg} alt={`logo-${team.name}`} />
+      </button>
     </>
   );
 }
